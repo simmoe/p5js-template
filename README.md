95e54f89425a09e8ef2106830fd1fae6e5c5e34d
simmoe <simmoe@gmail.com>
2023-06-01 14:02:29 +0200

    cmd

---
+++ README.md
@@ -0,0 +1,6 @@
+# p5.js tom template
+
+Dette er en tom p5.js template til brug i programmeringsprojekter. 
+Linker til til p5.js biblioteket via cdn 
+
+
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 
 
- function setup(){
-     createCanvas(windowWidth, windowHeight)
-     background('green')
-    }
+function setup(){
+}
 
 function draw(){
-    let green = map(mouseX, 0, windowWidth, 0, 255)
-    fill(0, green, 0)
-    ellipse(mouseX, mouseY, 20 )
 }
